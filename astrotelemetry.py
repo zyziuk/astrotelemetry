005e803fe7cb2fbfa0edf0eead6c42b13dcb6753
Zyziuk <zyziuk@gmail.com>
2021-04-17 17:48:55 +0200

    adding connection loop

--- astrotelemetry.py
+++ astrotelemetry.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('reporter')
+import time
 
 import socket
 import atexit
@@ -8,7 +9,7 @@ from statsReporter import StatsReporter
 from indiClientReporter import IndiClientReporter
 from influxFormatter import format_measurement_to_str_influx
 
-verbose = True
+verbose = False
 
 telegraf_ip = 'localhost'
 telegraf_udp_port = 8094
@@ -18,13 +19,18 @@ indi_tcp_port = 7624
 
 report_interval = 10
 
-
 reporter = StatsReporter((socket.AF_INET, socket.SOCK_DGRAM), (telegraf_ip, telegraf_udp_port),
                          formatter=format_measurement_to_str_influx, verbose=verbose)
 atexit.register(reporter.close_socket)
 
 indiclient = IndiClientReporter(reporter)
 indiclient.setServer(indi_ip, indi_tcp_port)
-indiclient.connectServer()
+
+while(indiclient.isServerConnected() == False):    
+    print("indiclient: Trying to connect ...")
+    indiclient.connectServer()
+    time.sleep(1)
+
+print("indiclient.connectServer(): OK")
 indiclient.reportAllIndiProps(report_interval)
 
